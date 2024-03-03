80630ef428512a70ec822dbbf8031802e5d05692
David Graat <david.graat@aah.org>
2024-03-02 21:58:18 -0600

    feat(elements)!: Feature 123 (#2)
    * BREAKING CHANGE: Some major refactoring in index.js
    
    * release.yml
    
    * feat(elements)!: major refactoring
    
    BREAKING CHANGE: refactored index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function helloWorld() {
-    console.log("HELLO World!");
+const index = {
+    helloWorld() {
+        console.log("Hello World!");
+    },
+    
+    goodNight() {
+        console.log("Goodnight World!");
+    }
 }
   
-export default helloWorld;
+export default index;
