a4a9eda666a2d2d3003bb2a56e4c24cab7625518
David Graat <david.graat@aah.org>
2024-03-15 09:54:56 -0500

    testing

---
+++ .releaserc.cjs
@@ -0,0 +1,56 @@
+const branch = process.env.GITHUB_REF_NAME;
+const rules = [
+	{ "type": "breaking", "release": "major" },
+	{ "type": "feature", "release": "minor" },
+	{ "type": "defect", "release": "patch" },
+	{ "type": "issue", "release": "patch" },
+	{ "type": "hotfix", "release": "patch" },
+];
+
+module.exports = {
+	branches: [
+		'main', 
+		{
+			"name": "task-**",
+			"prerelease": true
+		},
+		{
+			"name": "feature-**",
+			"prerelease": true
+	  	},
+		{
+			"name": "issue-**",
+			"prerelease": true
+	  	},
+		{
+			"name": "defect-**",
+			"prerelease": true
+		},
+		{
+			"name": "hotifix-**",
+			"prerelease": true
+		}
+	], 
+	plugins: branch === 'main' ? [
+		[
+			'@semantic-release/commit-analyzer', 
+			{
+				"preset": "angular",
+				"releaseRules": rules
+		  	}
+		],
+		'@semantic-release/release-notes-generator',
+		"@semantic-release/npm",
+		"@semantic-release/github",
+		] : [
+		[
+			'@semantic-release/commit-analyzer', 
+			{
+				"preset": "angular",
+				"releaseRules": rules
+			}
+		],
+		'@semantic-release/release-notes-generator',
+		"@semantic-release/npm",
+		],
+};
