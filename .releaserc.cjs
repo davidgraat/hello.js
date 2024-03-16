b95cc1a03dedc7fdcf057386ebe71c3d5d2bca6c
David Graat <dgraat@gmail.com>
2024-03-16 17:45:07 -0500

    feature(23): Z4 Test component

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -42,7 +42,7 @@ module.exports = {
 		[
 			'@semantic-release/release-notes-generator',
 			{
-			        'preset': 'angular',
+			        'preset': 'conventionalcommits',
 			        'presetConfig': {
 					'types': [
 			                 	{
